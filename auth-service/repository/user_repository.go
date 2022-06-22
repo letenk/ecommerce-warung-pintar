ea6ff8b20d19a2e56a979ae8183d63489742f852
Rizky Darmawan <darmawanrizky43@gmail.com>
2022-06-22 11:31:43 +0700

    feature: added repository find user by email

--- auth-service/repository/user_repository.go
+++ auth-service/repository/user_repository.go
@@ -7,6 +7,7 @@ import (
 
 type Repository interface {
 	Save(user domain.User) (domain.User, error)
+	FindByEmail(email string) (domain.User, error)
 }
 
 type repository struct {
@@ -20,6 +21,20 @@ func NewRepositoryUser(db *gorm.DB) *repository {
 func (r *repository) Save(user domain.User) (domain.User, error) {
 	// Create new user on db
 	err := r.db.Save(&user).Error
+	// If err return object data user, with error
+	if err != nil {
+		return user, err
+	}
+
+	// If success return new data user, with no error
+	return user, nil
+}
+
+func (r *repository) FindByEmail(email string) (domain.User, error) {
+	var user domain.User
+
+	// Find user by email
+	err := r.db.Where("email = ?", email).Find(&user).Error
 	// If err return object data user, with error
 	if err != nil {
 		return user, err
