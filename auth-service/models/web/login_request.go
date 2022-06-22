8a9e44f5164a4b6c281c4b3af946298210e2ee8b
Rizky Darmawan <darmawanrizky43@gmail.com>
2022-06-22 14:54:20 +0700

    refactor: field email must be format email

--- auth-service/models/web/login_request.go
+++ auth-service/models/web/login_request.go
@@ -1,6 +1,6 @@
 package web
 
 type LoginRequest struct {
-	Email    string `json:"email" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required,min=3"`
 }
